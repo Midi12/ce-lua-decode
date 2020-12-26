5bd4ee05916e276793caa8342dcb9082d3620a9f
Midi12 <Midi12@users.noreply.github.com>
2020-12-26 22:59:17 +0100

    feature(project): initial commit

---
+++ CEBase85.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+
+namespace ce_lua_decode
+{
+    public static class CEBase85
+    {
+        private static string _base85alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%()*+,-./:;=?@[]^_{}";
+
+        public static bool TryBase85ToBin(string input, ref MemoryStream output)
+        {
+            bool success = false;
+            
+            try {
+                Base85ToBin(input, ref output);
+            }
+            catch (Exception ex) {
+                success = false;
+            }
+
+            return success;
+        }
+
+        private static void Base85ToBin(string input, ref MemoryStream output)
+        {
+            int inputSize = input.Length;
+            int i = 0;
+            UInt32 value = 0;
+
+            while (i < inputSize)
+            {
+                char c = input[i];
+                value = Convert.ToUInt32(_base85alphabet.IndexOf(c)) * Convert.ToUInt32(Math.Pow(85, 4));
+                i += 1;
+
+                if (i < inputSize)
+                {
+                    c = input[i];
+                    value += Convert.ToUInt32(_base85alphabet.IndexOf(c)) * Convert.ToUInt32(Math.Pow(85, 3));
+                    i += 1;
+                }
+
+                if (i < inputSize)
+                {
+                    c = input[i];
+                    value += Convert.ToUInt32(_base85alphabet.IndexOf(c)) * Convert.ToUInt32(Math.Pow(85, 2));
+                    i += 1;
+                }
+
+                if (i < inputSize)
+                {
+                    c = input[i];
+                    value += Convert.ToUInt32(_base85alphabet.IndexOf(c)) * Convert.ToUInt32(Math.Pow(85, 1));
+                    i += 1;
+                }
+
+                if (i < inputSize)
+                {
+                    value += Convert.ToUInt32(_base85alphabet.IndexOf(c));
+                    i += 1;
+
+                    output.WriteByte(Convert.ToByte((value >> 24) & 0xFF));
+                    output.WriteByte(Convert.ToByte((value >> 16) & 0xFF));
+                    output.WriteByte(Convert.ToByte((value >> 8) & 0xFF));
+                    output.WriteByte(Convert.ToByte(value & 0xFF));
+                }
+            }
+
+            switch (inputSize % 5)
+            {
+                case 2:
+                    value += 84 * 85 + 84;
+                    output.WriteByte(Convert.ToByte((value >> 24) & 0xFF));
+                    break;
+                case 3:
+                    value += 84 * Convert.ToUInt32(Math.Pow(85, 2)) + 84 * 85 + 84;
+                    output.WriteByte(Convert.ToByte((value >> 24) & 0xFF));
+                    output.WriteByte(Convert.ToByte((value >> 16) & 0xFF));
+                    break;
+                case 4:
+                    value += 84;
+                    output.WriteByte(Convert.ToByte((value >> 24) & 0xFF));
+                    output.WriteByte(Convert.ToByte((value >> 16) & 0xFF));
+                    output.WriteByte(Convert.ToByte((value >> 8) & 0xFF));
+                    break;
+                default:
+                    break;
+            }
+
+            output.Seek(0, SeekOrigin.Begin);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using ICSharpCode.SharpZipLib;
+
+namespace ce_lua_decode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banner();
+
+            if (args.Length != 2)
+            {
+                Usage();
+            }
+            else
+            {
+                string arg0 = args[0];
+                string arg1 = args[1];
+
+                switch (arg0)
+                {
+                    case "-f":
+                        DecodeFile(arg1);
+                        break;
+                    case "-s":
+                        DecodeString(arg1);
+                        break;
+                    default:
+                        Console.WriteLine("Unknown switch");
+                        break;
+                }
+            }
+        }
+
+        private static void DecodeFile(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                Console.WriteLine("File not found");
+                return;
+            }
+
+            string content = File.ReadAllText(filename);
+            DecodeString(content);
+        }
+
+        private static void DecodeString(string str)
+        {
+            DateTime datetime = DateTime.Now;
+
+            using (FileStream fs = new FileStream($"output_{datetime.ToShortDateString().Replace("/", "")}_{datetime.ToShortTimeString().Replace(":", "")}.chunk", FileMode.Create))
+            {
+                MemoryStream ms = new MemoryStream();
+
+                CEBase85.TryBase85ToBin(str, ref ms);
+
+                using (ICSharpCode.SharpZipLib.Zip.Compression.Streams.InflaterInputStream zs = new ICSharpCode.SharpZipLib.Zip.Compression.Streams.InflaterInputStream(ms))
+                {
+                    zs.CopyTo(fs);
+                    fs.Flush();
+                }
+
+                ms.Dispose();
+            }
+        }
+
+        private static void Banner()
+        {
+            Console.WriteLine("ce-lua-decode");
+            Console.WriteLine("A toy to decode lua bytecode embedded into Cheat Engine \"decodeFunction\" parameters");
+        }
+
+        private static void Usage()
+        {
+            Console.WriteLine("\nUsage :");
+            Console.WriteLine("ce-lua-decode [-f filename | -s raw_string]");
+            Console.WriteLine("\tSwitch -f filename : run the program with a file containing the string to decode to bytecode");
+            Console.WriteLine("\tSwitch -s raw_string : run the program with a string to decode to bytecode");
+        }
+    }
+}
